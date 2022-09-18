9056450fafee926ee84a056b2329264a88ca11a0
AkinnimiManuel <akinnimiemmanuel@gmail.com>
2022-09-18 10:31:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+This is a clone of starbucks cofee page official website [www.starbucks.com]
+
+
+
+### Built with
+
+- Semantic HTML5 markup
+- css framework(tailwind css)
+- CSS custom properties
+- CSS flexbox
+- css grid
+
+### Screenshot
+
+![My Solution](./public/screenshots/Screenshot1.png) 
+![My Solution](./public/screenshots/Screenshot2.png)
+![My Solution](./public/screenshots/Screenshot3.png)
+
+## Author
+- Twitter - [@EMMYLASH_TINS](https://www.twitter.com/EMMYLASH_TINS)
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontSize:{
+        sm: ['14px'],
+      },
+      margin:{
+        xll:['40px'],
+      },
+      colors: {
+        starGreen: "#D4E9E2",
+        deepGreen: "#006241",
+        lightText: "rgb(0,0,0,0.58)",
+        titleText:"rgb(0,0,0,0.87)",
+      },
+    },
+  },
+  plugins: [],
+}
